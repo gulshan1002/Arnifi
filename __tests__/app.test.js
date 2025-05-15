cdfeb178b85a135a1df5b016d5beaad12891ae3c
Gulshan <gulshan190210@gmail.com>
2025-05-15 11:41:48 +0530

    Making testcases fail

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,4 +2,8 @@ describe('Application Tests', () => {
     test('hello world!', () => {
         expect(1 + 1).toBe(2);
     });
+
+    test('this test will fail', () => {
+        expect(2 * 2).toBe(5); // This will fail
+    });
 });
